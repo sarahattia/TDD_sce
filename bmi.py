c749ddb65a6c96dfbcb93fbafd945371df26f995
Ziv-Gimani <Zivgi@ac.sce.ac.il>
2020-01-15 18:03:00 +0200

    src

--- bmi.py
+++ bmi.py
@@ -1,32 +1,31 @@
 class BMI:
 
+	@staticmethod
 	def BMI_Calcul(high, weigth):
-		return weigth//(high**2)
+		result = weigth/(high**2)
+		return round(result, 2)
 
-	def result_BMI(high, weigth):
-		x = BMI.BMI_Calcul(high, weigth)
+	@staticmethod
+	def result_BMI(bmi):
 
-		if x < 18:
+		if bmi < 18.5:
 			return "Underweight"
 
-		if x >= 18 and x < 25:
+		if bmi >= 18.5 & bmi < 25:
 			return "Normal"
 
-		if x >= 25 and x < 30:
+		if bmi >= 25 & bmi < 30:
 			return "Overweight"
 
-		if x >= 30.0 and x < 35:
+		if bmi >= 30 & bmi < 35:
 			return "Obese class I"
 
-		if x >= 35 and x < 40:
+		if bmi >= 35 & bmi < 40:
 			return "Obese class II"
 
-		if x >= 40:
+		if bmi >= 40:
 			return "Obese class III"
 
 
 
-
-
-print(BMI.result_BMI(1.6, 52))
-# assignment11
+#assignment11
